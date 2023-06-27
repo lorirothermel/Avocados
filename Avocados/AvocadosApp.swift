58fb54eed7641002584d76ae7300313ea7d33828
LBRothermel <128320877+LBRothermel@users.noreply.github.com>
2023-06-27 16:07:00 -0400

    Initial Commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Lori Rothermel on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
